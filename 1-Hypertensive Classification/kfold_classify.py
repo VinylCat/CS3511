98729f65394ba8ed2899862e67febbbf3c3103e7
Zhiye Wang <94344088+VinylCat@users.noreply.github.com>
2024-04-28 21:04:50 +0800

    final version

--- 1-Hypertensive Classification/kfold_classify.py
+++ 1-Hypertensive Classification/kfold_classify.py
@@ -1,10 +1,13 @@
+# coding=utf-8
 import csv
 import os
 import shutil
 import math
 import cv2
 from random import sample
-
+import torchvision.transforms as transforms
+import numpy as np
+from PIL import Image
 # 定义路径变量
 images_path = '1-Images/1-Training Set/'
 groundtruths_path = '2-Groundtruths/HRDC Hypertensive Retinopathy Classification Training Labels.csv'
@@ -28,6 +31,24 @@ def data_augmentation(image_path, dest_path):
         mirrored_image = cv2.flip(image, 1)
         mirrored_image_path = f"{dest_path.rsplit('.', 1)[0]}_m.png"
         cv2.imwrite(mirrored_image_path, mirrored_image)
+        # img = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        # output = np.zeros(np.asarray(img).shape, np.uint8)
+        # prob = np.random.uniform(0.0005, 0.001)  # 随机噪声比例
+        # thres = 1 - prob
+        # image = np.asarray(img)
+        # for i in range(image.shape[0]):
+        #     for j in range(image.shape[1]):
+        #         rdn = np.random.random()
+        #         if rdn < prob:
+        #             output[i][j] = 0
+        #         elif rdn > thres:
+        #             output[i][j] = 255
+        #         else:
+        #             output[i][j] = image[i][j]
+        # nosie_image = transforms.ToPILImage()(output)
+        # noise_image_path = f"{dest_path.rsplit('.', 1)[0]}_noise.png"
+        # img = cv2.cvtColor(np.asarray(nosie_image), cv2.COLOR_RGB2BGR)
+        # cv2.imwrite(noise_image_path, img)
 
 # 确保train目录及其子目录0和1存在
 for label in ['0', '1']:
@@ -50,26 +71,6 @@ for image in os.listdir(images_path):
         dest_path = os.path.join(train_path, label, image)
         print(f"Copying {src_path} to {dest_path}")  # 打印出正在复制的文件路径
         shutil.copy(src_path, dest_path)
-
-for label in ['0', '1']:
-    os.makedirs(os.path.join(test_path, label), exist_ok=True)
-
-# 对每个分类执行操作
-for label in ['0', '1']:
-    # 计算需要移动的图片数量（约30%）
-    src_dir = os.path.join(train_path, label)
-    files = os.listdir(src_dir)
-    num_to_move = math.ceil(len(files) * 0.3)  # 使用math.ceil确保结果是整数
-
-    # 随机选择要移动的图片
-    files_to_move = sample(files, num_to_move)
-
-    # 移动选中的图片到测试目录
-    for file in files_to_move:
-        src_file_path = os.path.join(src_dir, file)
-        dest_file_path = os.path.join(test_path, label, file)
-        shutil.move(src_file_path, dest_file_path)
-        print(f"Moved {file} to {dest_file_path}")
 
 for label in ['0', '1']:
     for image in os.listdir(os.path.join(train_path, label)):
